2da402ba09c48b474cd8c675aa98aadf44d2a358
Mariia Nychyporenko <150645556+wrynych@users.noreply.github.com>
2024-02-06 02:38:26 +0000

    ch 1

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem "puma"
+gem "sinatra"
+
+gem "rackup", "~> 2.1"
+gem "sinatra-contrib"
---
+++ dice.rb
@@ -0,0 +1,10 @@
+
+require "sinatra"
+
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
